8ae65bc614e73c0135afa2f5f0e44e3e477ce339
LevyTavares <isaiaslevi2@gmail.com>
2025-10-27 20:17:31 -0300

    feat: implement controlled TextInput component with character count

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,18 @@
+import { useState } from 'react'
 import styles from './TextInput.module.css'
 
+
 export default function TextInput({ placeholder, maxLength, ...props }) {
+  const [text, setText] = useState('')
+
+  const onTextChange = (event) => {
+    setText(event.target.value)
+  }
+
   return (
     <div>
-        <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props} />
+        <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} value={text} onChange={onTextChange} {...props} />
+        <p>{text.length} / {maxLength}</p>
     </div>
   )
 }
