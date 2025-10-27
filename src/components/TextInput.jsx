754f93f9d48f9fe9dfa2a79da7686c9a6d4999ad
LevyTavares <isaiaslevi2@gmail.com>
2025-10-27 20:47:44 -0300

    feat: exibir lista de tweets enviados no componente TextInput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -21,6 +21,9 @@ export default function TextInput({ placeholder, maxLength, ...props }) {
         <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} value={text} onChange={onTextChange} {...props} />
         <p>{text.length} / {maxLength}</p>
         <button onClick={sendTweet}>Enviar Tweet</button>
+        {tweetList.map((tweet, index) => (
+          <p key={index}>{tweet}</p>
+        ))}
     </div>
   )
 }
