634a757520d11c477129e6dba04aa61280a8cf7d
LevyTavares <isaiaslevi2@gmail.com>
2025-10-27 20:44:16 -0300

    feat: adicionar funcionalidade de envio de tweet no componente TextInput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -5,14 +5,22 @@ import styles from './TextInput.module.css'
 export default function TextInput({ placeholder, maxLength, ...props }) {
   const [text, setText] = useState('')
 
+  const [tweetList, setTweetList] = useState([])
+
   const onTextChange = (event) => {
     setText(event.target.value)
+  }
+
+  function sendTweet() {
+    setTweetList([text, ...tweetList])
+    setText('')
   }
 
   return (
     <div>
         <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} value={text} onChange={onTextChange} {...props} />
         <p>{text.length} / {maxLength}</p>
+        <button onClick={sendTweet}>Enviar Tweet</button>
     </div>
   )
 }
